03ad9e73d42cf64e730c1b887d8de2001fe6e42a
Jhon Costa <jhon.costa.dev@gmail.com>
2023-02-18 16:27:59 -0300

    Selecionar palavra e ocultar
    Selecionra palavra do array categoria e exibir ocultada para o usuário

--- script.js
+++ script.js
@@ -14,7 +14,7 @@ const categorias = {
     profissoes: ["desenvolvedor", "advogado", "telefonista", "motorista", "suporte", "medico", "professor"],
     animais: ["gato", "cao", "cobra", "galinha", "cavalo", "peixe", "formiga", "abelha", "girafa"],
     cores: ["azul", "branco", "preto", "marrom", "roxo", "vermelho"]
-}
+};
 
 function retornaArrayCategorias(){
     return Object.keys(categorias);
@@ -28,4 +28,28 @@ function retornaCategoria(){
 
 function exibeCategoria(){
     categoria.innerHTML = retornaCategoria();
-}
+};
+
+function retornaNumAleatorio(max){
+    return Math.floor(Math.random() * max);
+};
+
+function definePalavraProposta(){
+    const arrayPalavras = categorias[categoria.innerHTML];
+    let indicePalavra = retornaNumAleatorio(arrayPalvras.length);
+    palavraProposta = arrayPalavras[indicePalavra];
+    ocultaPalavra();
+};
+
+
+function ocultaPalavra(){
+    let palavraOcultada = "";
+    for(let i = 0; i < palavraProposta.length; i++){
+        palavraOcultada += "-";
+    }
+    exibePalavaInterface(palavraOcultada);
+};
+
+function exibePalavaInterface(palavra){
+    palavraInterface.innerHTML = palavra;
+};
