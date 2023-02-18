01bf92e42a92e10e1405144ea11e73418b1d7f34
Jhon Costa <jhon.costa.dev@gmail.com>
2023-02-18 17:07:35 -0300

    Finalizado
    Finalizadfo 18/02

--- script.js
+++ script.js
@@ -1,10 +1,20 @@
+const categoria = document.querySelector("#category");
+const letrasErradas = document.querySelector(".wrongLetters");
+   const palavraInterface = document.querySelector(".dashes");
+const olhos = Array.from(document.querySelectorAll(".eyes"));
 
+let partesBoneco = Array.from(document.querySelectorAll("#person div"));
+partesBoneco = partesBoneco.slice(2, partesBoneco.length);
 
+let palavraProposta;
 
+let letrasErradasArray = [];
 
+let indiceBoneco;
 
+const numTentativas = 7;
 
-
+const opacidadeOlhos = 0.3;
 
 
 //Criando categorias
@@ -61,7 +71,7 @@ function tentativa(letra){
     }else{
         letrasErradasArray.push(letra);
         letrasErradas.innerHTML = "Letras erradas: " + letrasErradasArray;
-        
+
     }
 };
 
@@ -86,3 +96,34 @@ function atualizarPalavraInterface(letra){
 function retornaLetra(e){
     tentativa(e.key);
 };
+
+function desenhaBoneco(){
+    partesBoneco[indiceBoneco].classList.remove("hide");
+    indiceBoneco++; 
+}
+
+function desenhaOlhos(){
+    olhos.forEach((olho => {
+        olho.style.opacity = 1;
+        olho.style.zIndex = 10;
+    }));
+}
+
+
+function ocultaBoneco(){
+    olhos.forEach((olho => {
+        olho.style.opacity = opacidadeOlhos; 
+    }));
+    partesBoneco.forEach(parteBoneco => {
+        parteBoneco.classList.add("hide");
+    });
+}
+
+function iniciaJogo(){
+    indiceBoneco = 0;
+    letrasErradasArray = [];
+    letrasErradas.innerHTML = "Letras erradas: ";
+    window.addEventListener("keypress", retornaLetra);
+}
+
+window.addEventListener("load", iniciaJogo);
